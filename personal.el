3d23352196cf7ff2ccfbd939e5a422cd04e4d62a
Dragomir Todorov <dragomir.todorov@circles.bz>
2017-07-25 11:28:38 +0300

    Add personal.el to emacs configuration files

---
+++ personal.el
@@ -0,0 +1,104 @@
+;; install additional package for C/C++ development
+(defvar package-list)
+(setq package-list '(ag
+                     rtags
+                     irony
+                     clang-format
+                     company-rtags
+                     company-irony
+                     company-irony-c-headers
+                     flycheck-rtags))
+
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install the new packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+;; Require flycheck to be present
+(require 'flycheck)
+;; Force flycheck to always use c++11 support. We use
+;; the clang language backend so this is set to clang
+(add-hook 'c++-mode-hook
+          (lambda () (setq flycheck-clang-language-standard "c++11")))
+;; Turn flycheck on everywhere
+(global-flycheck-mode)
+
+;; clang-format can be triggered using C-M-tab
+(require 'clang-format)
+(global-set-key [C-M-tab] 'clang-format-region)
+
+;; Create clang-format file using Webkit style
+;; cp ~/work/clang/clang-format.txt ~/.clang-format
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set up code completion with company and irony
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable semantics mode for auto-completion
+(require 'cc-mode)
+(require 'semantic)
+;; (global-semanticdb-minor-mode 1)
+;; (global-semantic-idle-scheduler-mode 1)
+;; (semantic-mode 1)
+
+;; Setup irony-mode to load in c-modes
+(require 'irony)
+(require 'company-irony-c-headers)
+;;(require 'cl)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; irony-mode hook that is called when irony is triggered
+(defun my-irony-mode-hook ()
+  "Custom irony mode hook to remap keys."
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; company-irony setup, c-header completions
+(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+;; Remove company-semantic because it has higher precedance than company-clang
+;; Using RTags completion is also faster than semantic, it seems. Semantic
+;; also provides a bunch of technically irrelevant completions sometimes.
+;; All in all, RTags just seems to do a better job.
+(setq company-backends (delete 'company-semantic company-backends))
+;; Enable company-irony and several other useful auto-completion modes
+;; We don't use rtags since we've found that for large projects this can cause
+;; async timeouts. company-semantic (after company-clang!) works quite well
+;; but some knowledge some knowledge of when best to trigger is still necessary.
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends '(company-irony-c-headers
+                        company-irony
+                        company-clang
+                        )
+    )
+)
+
+;; Zero delay when pressing tab
+;; (setq company-idle-delay 0)
+;; (define-key c-mode-map [(tab)] 'company-complete)
+;; (define-key c++-mode-map [(tab)] 'company-complete)
+;; Delay when idle because I want to be able to think
+;; (setq company-idle-delay 0.2)
+
+;; Prohibit semantic from searching through system headers. We want
+;; company-clang to do that for us.
+(setq-mode-local c-mode semanticdb-find-default-throttle
+                 '(local project unloaded recursive))
+(setq-mode-local c++-mode semanticdb-find-default-throttle
+                 '(local project unloaded recursive))
+
+(semantic-remove-system-include "/usr/include/" 'c++-mode)
+(semantic-remove-system-include "/usr/local/include/" 'c++-mode)
+(add-hook 'semantic-init-hooks
+          'semantic-reset-system-include)
